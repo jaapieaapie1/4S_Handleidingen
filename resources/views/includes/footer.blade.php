4c4c20cd71e0565e39ecaa56d81b69e9a409c648
jaap <jaapelst@gmail.com>
2021-10-19 11:06:22 +0200

    changed to button

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,10 +1,32 @@
 
 <footer>
 	© {{ __('misc.copyright') }}
+
+    <div class="footer-box">
+        <div>
+            <h4>Over ons</h4>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci autem cum eius esse possimus, quas repellendus velit! Aliquam, aspernatur consequatur debitis earum laudantium recusandae saepe totam vitae? Consequatur, porro?
+            </p>
+        </div>
+
+        <div>
+            <h4>Contact</h4>
+            <p>geert@gmail.com</p>
+            <p>Sprundelsebaan 1a<br>Zundert 4882JB</p>
+        </div>
+
+        <div>
+            <h4>Social links</h4>
+            <a href="">Facebook</a>
+            <a href="">Instagram</a>
+            <a href="">Twitter</a>
+        </div>
+    </div>
 </footer>
 
 
-<!-- analytics code -->              
+<!-- analytics code -->
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
@@ -19,10 +41,10 @@
 </script>
 <!-- Einde analytics code -->
 
-<script language="Javascript" type="text/javascript"> 
- <!-- 
- if (top.location!= self.location) { 
-  top.location = self.location.href
- } 
-//--> 
+<script language="Javascript" type="text/javascript">
+{{-- --}}
+// if (top.location!= self.location) {
+//  top.location = self.location.href
+// }
+
 </script>
